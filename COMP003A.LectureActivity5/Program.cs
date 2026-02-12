153de5bdb62b454678317a60e4d753cab34af96d
evanbern59 <evansantos1202@gmail.com>
2026-02-11 20:16:41 -0800

    finished

--- COMP003A.LectureActivity5/Program.cs
+++ COMP003A.LectureActivity5/Program.cs
@@ -11,7 +11,7 @@
             int studentAge = 21;
             DisplayUserInfo(studentName, studentAge);
 
-            int total = calcukatesum(9, 6);
+            int total = CalculateSum(9, 6);
             Console.WriteLine("The sum is: " + total);
 
             int mainValue = 100;
@@ -19,12 +19,32 @@
             if (true)
             {
                 int insideBlock = 25;
-                Console.WriteLine("Inside block: " insideBlock);
+                Console.WriteLine("Inside block: " + insideBlock);
 
+            }
+
+            for (int counter = 0; counter < 4; counter++)
+            {
+                Console.WriteLine("Counter value: " + counter);
             }
 
 
             
+        }
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to Module 5");
+        }
+
+        static void DisplayUserInfo(string name, int age)
+        {
+            Console.WriteLine("Name: " + name + ", Age: " + age);
+        }
+
+        static int CalculateSum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
         }
     }
 }
